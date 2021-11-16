4a1637c6109a590b69d9b3498d539b413bb6e03d
Kacper Hes <kacperhes@MacBook-Air-Kacper.local>
2021-11-16 09:41:44 +0100

    Add project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { ReactElement } from "react";
+import logo from "./icons/logo.svg";
+import "./styles/App.css";
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
